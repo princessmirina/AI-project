045e14f48e9896a330805fcac3d5ffe59f7e5aab
princessmirina <85294705+princessmirina@users.noreply.github.com>
2024-10-22 23:11:36 -0700

    added javascript and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: [
+      "You Are the Penultimate Love of My Life.",
+      "The garden you plant and I plan is tunneled through by voles the vowels. ",
+      "we speak aren't vows but there's something holding me here, for now, ",
+      " like your eyes, which I suppose are brown, after all.",
+    ],
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", generatePoem);
